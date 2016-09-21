ce517bca7c2bb5acc5c3bd68510b14e95c783f76
Hunter <h90barron@gmail.com>
2016-09-21 11:06:40 -0500

    "Remove unused lib"

--- freq_bin.py
+++ freq_bin.py
@@ -1,16 +1,15 @@
 import pyaudio
 import wave
-import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.fftpack import fft
 from pylab import*
 
-"""
-Attempting to filter through freq bins which will then be stored in a history
-buffer for that freq bin. The history buffer will then be used to compute the
-local energy max of that give freq range and determine if it is a beat or not in
-that range.
-"""
+
+# Attempting to filter through freq bins which will then be stored in a history
+# buffer for that freq bin. The history buffer will then be used to compute the
+# local energy max of that give freq range and determine if it is a beat or not in
+# that range. Initially using constant audio frequency for testing. 
+
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -34,7 +33,7 @@ print("* recording")
 frames = []
 frequency_data = []
 
-
+# Only one bin should contain data while using constant freq
 
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
 #for i in range(1):
@@ -52,68 +51,13 @@ for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     freq = freqs[idx]
     freq_in_hertz = abs(freq * RATE)
     print(freq_in_hertz)
-    
     #--------------------------------------------------------
-##    print(len(raw_transform))
-##    freqs = fftfreq(len(raw_transform))
-##    print(len(freqs))
-##    idx = np.argmax(np.abs(raw_transform))
-##    #print(freqs)
-##    freq = freqs[idx]
-##    freq_in_hertz = abs(freq * RATE)
-##    frequency_data.append(freq_in_hertz)
-##    #print(freq_in_hertz)
-    
-##decoded = np.fromstring(data, dtype=np.int32)
-##coef = decoded.flatten()
-##print(coef)
-##raw_data = [(x/2**8.)*2-1 for x in coef]
-##raw_transform = fft(raw_data)
-##freqs = fftfreq(len(raw_transform))
-##idx = np.argmax(np.abs(raw_transform))
-##print(freqs)
-##freq = freqs[idx]
-##freq_in_hertz = abs(freq * RATE)
-##frequency_data.append(freq_in_hertz)
-##print(freq_in_hertz)
-    #frames.append(data)
-
-
-##        audio = np.fromstring(data, dtype=np.int16)
-##    audio = audio.flatten()
-##    left,right = np.split(np.abs(np.fft.fft(audio)), 2)
-##    y = np.add(left,right[::-1])
-##    x = np.arange(CHUNK/2, dtype=float)
-##    i = int((CHUNK/2)/10)
-##    y = y[:i]
-##    x = x[:i]*(RATE/CHUNK)
-##    y = y/float(100)
-##    print(y)
 print("* done recording")
-
 
 stream.stop_stream()
 stream.close()
 p.terminate()
 
-##for i in range(0, len(frequency_data), 10):
-##    print(frequency_data[i])
 
-##wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-##wf.setnchannels(CHANNELS)
-##wf.setsampwidth(p.get_sample_size(FORMApT))
-##wf.setframerate(RATE)
-##wf.writeframes(b''.join(frames))
-##wf.close()
-##
-##fs, input_data = wavfile.read('output.wav')
-##data = input_data.T[0]
-##raw_data = [(ele/2**8.)*2-1 for ele in data]
-##raw_transform = fft(raw_data)
-##freqs = fftfreq(len(raw_transform))
-##print(freqs.min(), freqs.max())
-##
-##idx = np.argmax(np.abs(raw_transform))
-##freq = freqs[idx]
-##freq_in_hertz = abs(freq * RATE)
-##print(freq_in_hertz)
+
+
